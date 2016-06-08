8a608097273efd0e092518207cf98e22fd41dda3
Danny <dannycornelisse@hotmail.com>
2016-06-08 16:21:59 +0200

    added movement of platform

--- Canvas breakout Game/js/move-platform.js
+++ Canvas breakout Game/js/move-platform.js
@@ -1,33 +1,64 @@
 document.addEventListener('DOMContentLoaded', function(){
-	createPlatform()
+	var canvas  = document.getElementById("canvas");;
+	var platform, 
+		ball;
+
+	createPlatform();
+	createBall();
+
 
 
 }, false)
 
-var canvas;
-var platform;
+
 
 function createPlatform(){
-	var canvas = document.getElementById("canvas");
-	var platform = canvas.getContext("2d");
+	platform = canvas.getContext("2d");
 	platform.fillStyle = "#003366";
-	platform.fillRect(200-20,350,40,10);
+	platform.width = 50;
+	platform.height = 10;
+	platform.x = canvas.width/2 - platform.width/2;
+	platform.y  = 350;
+	
+	platform.fillRect(platform.x, platform.y,platform.width,platform.height);
 
-	canvas.addEventListener( "keydown", movePlatform, true);
+	platform.dx;
+
+	canvas.addEventListener("keydown", movePlatform);
 
 	
 };
 
-function movePlatform(event) {
+function createBall(){
+    ball= canvas.getContext('2d');
+    ball.beginPath();
+    ball.fillStyle="#003366";
+    // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+    ball.arc(95,50,7,0,2*Math.PI,true);
+    ball.fill();
+}
 
-		console.log(event.keyCode);
+
+
+function movePlatform(event) {
+		console.log(platform);
 		var key = event.keyCode;
 
 		if (key === 37 || key === 65){
-			//move platform left
+			platform.dx = -5;
+			platform.x += platform.dx;
+			platform.clearRect(platform.x,platform.y, 100,10);
+			platform.fillRect(platform.x, platform.y,50,10);
 		}
 
 		if (key === 39 || key === 68){
+			platform.dx = 5;
+			platform.x += platform.dx;
+			platform.clearRect(platform.x,platform.y, -100,10);
+			platform.fillRect(platform.x, platform.y,50,10);
+
 			
 		}
+
+
 	};
