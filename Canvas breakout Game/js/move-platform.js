bba33ee43c9fe5c7289dcf00c7aa51fbc179dff9
Danny <dannycornelisse@hotmail.com>
2016-06-08 11:53:33 +0200

    added keydown event listner

--- Canvas breakout Game/js/move-platform.js
+++ Canvas breakout Game/js/move-platform.js
@@ -1,4 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-	createPlatform();
-});
+	createPlatform()
 
+
+}, false)
+
+var canvas;
+var platform;
+
+function createPlatform(){
+	var canvas = document.getElementById("canvas");
+	var platform = canvas.getContext("2d");
+	platform.fillStyle = "#003366";
+	platform.fillRect(200-20,350,40,10);
+
+	canvas.addEventListener( "keydown", movePlatform, true);
+
+	
+};
+
+function movePlatform(event) {
+
+		console.log(event.keyCode);
+		var key = event.keyCode;
+
+		if (key === 37 || key === 65){
+			//move platform left
+		}
+
+		if (key === 39 || key === 68){
+			
+		}
+	};
